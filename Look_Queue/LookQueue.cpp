6c0abae58d380eb324af6909b27c5bb5ba81ebe3
Ethan Edwards <evanevansonbusiness@gmail.com>
2023-11-27 11:30:57 -0800

    System switch

--- Look_Queue/LookQueue.cpp
+++ Look_Queue/LookQueue.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "LookQueue.h"
+
+void LookQueue::addRequest(Request *request, int cRWHeadTrack, int cRWHeadSector) {
+    LookQueueNode *newNode = new LookQueueNode(request);
+    LookQueueNode *currNode = head;
+    LookQueueNode *prevNode = nullptr;
+    bool added = false;
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    while (currNode != nullptr) {
+        if (currNode->request()->track() > cRWHeadTrack) {
+            if (prevNode == nullptr) {
+                newNode->next(currNode);
+                head = newNode;
+                added = true;
+                break;
+            } else {
+                newNode->next(currNode);
+                prevNode->next(newNode);
+                added = true;
+                break;
+            }
+        }
+        prevNode = currNode;
+        currNode = currNode->next();
+    }
+
+    if (!added) {
+        tail->next(newNode);
+        tail = newNode;
+    }
+}
